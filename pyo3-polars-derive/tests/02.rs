8fbb2c300ef842ea48805db10cb28e8447b29e9e
Matt S <mshepit@gmail.com>
2025-05-07 23:51:16 +1000

    suppress cfg warnings

--- pyo3-polars-derive/tests/02.rs
+++ pyo3-polars-derive/tests/02.rs
@@ -2,10 +2,9 @@ use polars_core::error::PolarsResult;
 use polars_core::prelude::*;
 use pyo3_polars_derive::polars_expr;
 
+#[allow(unused_variables)]
 #[polars_expr(output_type=Int32)]
 fn horizontal_product(series: &[Series], kwargs: Option<&str>) -> PolarsResult<Series> {
-    let _ = kwargs;
-
     let mut acc = series[0].clone();
     for s in &series[1..] {
         acc = (&acc * s)?
